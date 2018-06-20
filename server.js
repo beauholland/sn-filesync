b21853d6536fc263309fc36758ae2e8c1312c153
Abhishek Jagtap <abhijagtap1997@gmail.com>
2018-06-21 09:58:51 +1000

    Users can now select to start watching at the start
    Users no longer need to click "Open in Editor" to start watching for changes. If they wish, they can start it before downloading a file

--- server.js
+++ server.js
@@ -2,77 +2,102 @@
 var Server = require('ws').Server;
 var port = process.env.PORT || 9030;
 var ws = new Server({port: port});
-/* module to use to we can spawn child processes */
+
+// module to use to we can spawn child processes
 const {spawn} = require('child_process');
-var watching = false;
+
+// store the list of instances we're watching 
 var envs = [];
 
-
-console.log("\n==========Server started!==========\n");
-
-/* Listen for someone who wants to sync */
-ws.on('connection', function(w){
-  
-  /* Once the syn button has been clicked */
-  w.on('message', function(msg){
-  	// this is the received object which has all the required info
-    var received = JSON.parse(msg);
-    
-    // TESTING
-    console.log("Env \t ==> " + received.env);
-    console.log("Table \t ==> " + received.table);
-    console.log("sys_id \t ==> " + received.sys_id);
-
-    
-    // Let the ServiceNow instance know that we've received the data
-    // So close the connection
-    w.send("Server has received the following: " + msg);
-    var env = received.env.split('.')[0];
-    downloadFile(env, received.table, received.sys_id);
-    if(!watching || envs.includes(env)) {
-        envs.push(env);
-        watching = true;
-        startWatchingForChanges(env);
-    }
-  });
-  
-  w.on('close', function() {
-    console.log('\nclosing connection\n');
-  });
-
+// Read stdin
+var readline = require('readline');
+var read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
 
-function startWatchingForChanges(env) {
-    // spawn another child to run npm run watch-<env>
-    var watch = spawn('cmd', ['/c', 'npm run watch-' + env + ' --color']);
+/*
+ * What if user wants to start where he/she left off?
+ * So ask the user if they want to start a watch for some instance.
+ */
+read.question('Start watching an instance?(y/n)', (answer) => {
+    if(answer.match(/^y(es)?$/i)) {
+        console.log("Choose an instance: ");
+        var options = ['rttmsdev','riotintodev'];
+        for(var i = 0; i < options.length; i++) {
+            console.log("" + (i+1) + ". " + options[i]);
+        }
+        read.question('Instance Name: ', (instance) => {
+            var selection = parseInt(instance) - 1;
+            if(selection >=0 && selection <= options.length) {
+                envs.push(options[selection]);
+                startWatchingForChanges(options[selection]);
+            }
+            read.close();
+            main();
+        });
+    } else {
+        read.close();
+        main();
+    }
+});
 
-    watch.stdout.on('data', (data) => {
-        console.log(`STDOUT: ${data}`);
+/*
+ * Main, this is where you start listening for a "Open in Editor" Button press
+ */
+function main() {
+    console.log("\n==========Server Started!==========\n");
+    /* Listen for someone who wants to sync */
+    ws.on('connection', function(w){
+  
+        /* Once the syn button has been clicked */
+        w.on('message', function(msg){
+            // this is the received object which has all the required info
+            var received = JSON.parse(msg);
+            
+            // TESTING (print what was received)
+            console.log("Env \t ==> " + received.env);
+            console.log("Table \t ==> " + received.table);
+            console.log("sys_id \t ==> " + received.sys_id);
+
+            
+            var env = received.env.split('.')[0];
+            downloadFile(env, received.table, received.sys_id);
+            if(!envs.includes(env)) {
+                envs.push(env); // If you're already watching this instance, only start 1 watcher
+                startWatchingForChanges(env);
+            }
+
+            // Let the ServiceNow instance know that we've received the data
+            // So close the connection
+            w.send("Server has received the following: " + msg);
+
+        });
+  
+        w.on('close', function() {0
+            console.log('\nclosing connection\n');
+        });
+
     });
 
-    watch.on('close', (code) => {
-        console.log("WATCHING EXITED");
-        process.exit(0);
-    });
 }
 
 /*
- * Function to call which will do the syncing
- *
- * This is where you can call
- * npm run search-<instance>
- * npm run watch-<instance>
- * npm run resync-<instance>
- * etc
+ * Download the file, i.e use the given sys_id and table name
+ * to find the js code and download it locally
+ * Once downloaded will attempt to open the file in preferred editor
  */
 function downloadFile(env, table, sys_id) {
     var config = env+".config.json";
     var options = '--config ' + config + ' --pull "' + sys_id +'" --table ' + table;
-    
+
+    // Process which is responsible for downloading the file
     var ls = spawn('cmd', ['/c', 'node bin/app.js ' + options]);
 
+    // file to open
     var file = "";
 
+    // Redirect the stdout to parent processes stdout.
     ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
         var string = `${data}`;
@@ -82,19 +107,19 @@ function downloadFile(env, table, sys_id) {
             file = "./" + string.split('Saved file')[1].replace(" ", "");
             file = file.replace("\n", "");
             file = '"' + file + '"';
-            console.log("<FILE: >" + file);
-        } 
+            console.log("FILE: " + file);
+        }
     });
 
+    // redirect stderr from child process
     ls.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
     });
 
-    // Once the child process exist
+    // Once the child process exist (i.e once download is finished)
     // open the downloaded file in the sublime by calling another program :P
     ls.on('close', (code) => {
-
-        console.log(`child process exited with code ${code}`);
+        console.log(`Downloading process exited with code ${code}`);
         var pref = require('./editorConfig.json');
 
         // If preferred editor is vsCode then no need to call the module
@@ -104,13 +129,42 @@ function downloadFile(env, table, sys_id) {
         } else {
             // preferred is sublime or something else, so use open-in-editor module
             var editor = spawn('cmd', ['/s', '/c', 'node openInEditor.js ' + file], {windowsVerbatimArguments: true});
-        } 
+        }
 
-        //process.exit(0); // dont close the parent process lol 
+        editor.stdout.on('data', (data) => {
+            console.log("EDITOR: " + data);
+        });
+
+        editor.on('close', (d) => {
+            console.log("OPENING EDITOR PROCESS EXITED");
+        });
+
+        //process.exit(0); // dont exit the parent process lol
     });
 
     ls.on('error', (error) => {
         console.log(`child process errored :( ${error}`);
         process.exit(1); 
+    });
+}
+
+
+/*
+ * Start a watch-<instance> process to watch for changes
+ */
+function startWatchingForChanges(env) {
+    // spawn another child to run npm run watch-<instance>
+    process.env.FORCE_COLOR = true;
+
+    //"inherit" so that we can keep the colours
+    var watch = spawn('cmd', ['/c', 'npm run watch-' + env], {stdio: "inherit"});
+
+    // watch.stdout.on('data', (data) => {
+    //     process.stdout.write(data);
+    // });
+
+    watch.on('close', (code) => {
+        console.log("WATCHING PROCESS EXITED");
+        process.exit(0);
     });
 }
